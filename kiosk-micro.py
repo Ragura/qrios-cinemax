13f13996eb0ca69dc0e429b48400f2dc6acca975
Steven Schoovaerts <ragura@gmail.com>
2021-05-21 14:14:38 +0200

    Small bug fixes to button enable/disable state

--- kiosk-micro.py
+++ kiosk-micro.py
@@ -15,8 +15,6 @@ sg.theme('DarkBlue4')
 
 vertoningen_vandaag = dm.vertoningen_vandaag()
 films = list({vertoning.film for vertoning in vertoningen_vandaag})
-for film in films:
-    film.load_api_data()
 
 logo = sg.Image(data=get_img_data(
     "logo_wit.png", maxsize=(50, 50), first=True, local=True))
@@ -56,29 +54,15 @@ kolom_rechts = [
     ]
 ]
 
-kolom_beschrijving = [
-    [
-        sg.Text("DUUR", size=(3, 1), font="Helvetica 14",
-                key="-DUUR-", pad=((5, 0), (0, 0))),
-        sg.Text("min.", size=(5, 1), font="Helvetica 14",
-                key="-MINUTEN-", pad=((0, 0), (0, 0))),
-        sg.Text("KINDEREN NIET TOEGELATEN", font="Helvetica 14",
-                key="-KNT-")
-    ],
-    [sg.Text("BESCHRIJVING", size=(50, 10), key="-BESCHRIJVING-")]
-]
-
 layout = [
     [sg.Frame(title="", layout=[
         [logo, sg.Text("CINEMAX", font="Helvetica 30")]], pad=(0, 20))],
-    [sg.Column(kolom_links), sg.Column(kolom_rechts)],
-    [sg.Frame("TITEL FILM", layout=kolom_beschrijving,
-              k="-TITEL-", visible=False)]
+    [sg.Column(kolom_links), sg.Column(kolom_rechts)]
 ]
 
 global window
 window = sg.Window('Cinemax', layout, size=(
-    640, 720), element_justification="center", font="Helvetica 16", element_padding=(5, 5))
+    640, 480), element_justification="center", font="Helvetica 16", element_padding=(5, 5))
 
 
 def update_prijs(vertoning: Vertoning) -> None:
@@ -108,12 +92,6 @@ while True:
     if event == "-FILMS-":
         huidige_film = values["-FILMS-"][0]
         huidige_vertoning = None
-        window["-TITEL-"].update(value=huidige_film.titel, visible=True)
-        window["-DUUR-"].update(value=huidige_film.duur)
-        window["-KNT-"].update("KINDEREN NIET TOEGELATEN" if huidige_film.knt else "")
-        window["-RIJ_KINDEREN-"].update(visible=not bool(huidige_film.knt))
-        window["-BESCHRIJVING-"].update(huidige_film.beschrijving)
-
         vertoningen = [
             vertoning for vertoning in vertoningen_vandaag if vertoning.film == huidige_film]
         window["-VERTONINGEN-"].update(values=vertoningen)
@@ -121,7 +99,6 @@ while True:
         window["-KINDEREN-"].update(value=0)
         window["-VOLWASSENEN-"].update(value=0)
         window["-PRIJS-"].update("")
-        window["-KOPEN-"].update(disabled=True)
 
     if event == "-VERTONINGEN-":
         huidige_vertoning = values["-VERTONINGEN-"][0]
@@ -133,24 +110,18 @@ while True:
             update_prijs(huidige_vertoning)
 
     if event == "-KOPEN-":
-        if not huidige_vertoning:
-            continue
-
-        aantal_volwassenen = int(values["-VOLWASSENEN-"])
-        aantal_kinderen = int(values["-KINDEREN-"])
-        prijs_volwassene = Ticket.bereken_prijs(huidige_vertoning, False)
-        prijs_kind = Ticket.bereken_prijs(huidige_vertoning, True)
-        tickets = [Ticket(huidige_vertoning, True, prijs_kind) for _ in range(
-            aantal_kinderen)] + [Ticket(huidige_vertoning, False, prijs_volwassene) for _ in range(aantal_volwassenen)]
-        dm.insert_tickets(tickets)
-        sg.Window("Tickets gekocht", [[sg.Text("Geniet van de film!", font="Helvetica 20")]], element_justification="center",
-                  auto_close=True, auto_close_duration=3, margins=(50, 50), keep_on_top=True, no_titlebar=True).read(close=True)[0]
-        huidige_film = None
-        huidige_vertoning = None
-        window["-FILMS-"].update(set_to_index=-1)
-        window["-VERTONINGEN-"].update(set_to_index=-1)
-        window["-KINDEREN-"].update(value=0)
-        window["-VOLWASSENEN-"].update(value=0)
-        window["-PRIJS-"].update(value="")
-        window["-TITEL-"].update(visible=False)
-        window["-KOPEN-"].update(disabled=True)
+        if (huidige_vertoning):
+            aantal_volwassenen = int(values["-VOLWASSENEN-"])
+            aantal_kinderen = int(values["-KINDEREN-"])
+            prijs_volwassene = Ticket.bereken_prijs(huidige_vertoning, False)
+            prijs_kind = Ticket.bereken_prijs(huidige_vertoning, True)
+            tickets = [Ticket(huidige_vertoning, True, prijs_kind) for _ in range(
+                aantal_kinderen)] + [Ticket(huidige_vertoning, False, prijs_volwassene) for _ in range(aantal_volwassenen)]
+            dm.insert_tickets(tickets)
+            huidige_film = None
+            huidige_vertoning = None
+            window["-FILMS-"].update(set_to_index=-1)
+            window["-VERTONINGEN-"].update(set_to_index=-1)
+            window["-KINDEREN-"].update(value=0)
+            window["-VOLWASSENEN-"].update(value=0)
+            window["-PRIJS-"].update(value="")
